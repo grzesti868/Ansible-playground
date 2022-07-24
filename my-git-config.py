90feec42191aab3a04aa69719002faf1025b9caf
Greg <ansible@localhost.local>
2022-07-24 14:11:25 +0200

    added file and script v2

--- my-git-config.py
+++ my-git-config.py
@@ -2,7 +2,7 @@
 import os
 cmd = '''
 git config --global user.name "Greg"
-git config --global --add user.mail "ansible@localhost.local"
+git config --global --add user.email "ansible@localhost.local"
 '''
 os.system(cmd)
 
